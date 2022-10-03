d371ab0a7367e268f0d6f715a92488cf92761cc3
narhiril <roxannarusbarsky@yahoo.com>
2022-10-03 13:38:19 -0500

    part 2

---
+++ HelloASPDotNET/Controllers/EventsController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace HelloASPDotNET.Controllers
+{
+    public class EventsController : Controller
+    {
+        public static List<string> events = new List<string>();
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            events.Add("Halloween");
+            events.Add("Rocket Launch");
+            events.Add("Relatives Visiting");
+            events.Add("LaunchCode");
+            ViewBag.events = events;
+
+            return View();
+        }
+    }
+}
